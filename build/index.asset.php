b6f146a26c82f2bbaaa384d09e16fe0f4b8f1473
capbussat <26734622+capbussat@users.noreply.github.com>
2023-01-27 15:48:33 +0100

    Working

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '2476af3fed4492044ecd');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '47285ac397d89ed8dd7f');
