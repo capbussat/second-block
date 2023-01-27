9870b68aeffe165183810ffe67e2a824d5231d67
capbussat <26734622+capbussat@users.noreply.github.com>
2023-01-27 10:46:20 +0100

    Correcting error InnerBlock undefined
    Correcting error InnerBlock undefined

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '9c1b231ccc8a5c54f49e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e462119a41eb553c1ab8');
